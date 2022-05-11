b95e4c9d9a10e533546f57c2e31df2bfb0d995ed
Winston Jodjana <winj@cs.washington.edu>
2022-05-10 21:00:17 -0700

    Quick update about using BST in uMessage

--- src/main/java/chat/uMessage.java
+++ src/main/java/chat/uMessage.java
@@ -19,10 +19,11 @@ import java.util.function.Supplier;
 public class uMessage {
     private static final int N = 3;
     private static final String CORPUS = "corpus/eggs.txt";
+    // Use .binarySearchTreeConstructor(); if you want to test things since it is an implementation we provide and is guaranteed to work
     private static final Supplier<Dictionary<NGram, Dictionary<AlphabeticString, Integer>>> NEW_OUTER = NGramTester
-            .trieConstructor(NGram.class);
+            .binarySearchTreeConstructor();
     private static final Supplier<Dictionary<AlphabeticString, Integer>> NEW_INNER = NGramTester
-            .trieConstructor(AlphabeticString.class);
+            .binarySearchTreeConstructor();
 
     /*
      *

983607b46c67a912b0499a04ff09c4ef80fcf70d
Victor Liu <vkliu@uw.edu>
2024-05-02 05:25:52 -0700

    Implemented equals in CircularArrayFIFOQueue.

--- src/main/java/datastructures/worklists/CircularArrayFIFOQueue.java
+++ src/main/java/datastructures/worklists/CircularArrayFIFOQueue.java
@@ -97,18 +97,37 @@ public class CircularArrayFIFOQueue<E extends Comparable<E>> extends FixedSizeFI
     @SuppressWarnings("unchecked")
     public boolean equals(Object obj) {
         // You will finish implementing this method in project 2. Leave this method unchanged for project 1.
-        if (this == obj) {
-            return true;
-        } else if (!(obj instanceof FixedSizeFIFOWorkList<?>)) {
+//        if (this == obj) {
+//            return true;
+//        } else if (!(obj instanceof FixedSizeFIFOWorkList<?>)) {
+//            return false;
+//        } else {
+//            // Uncomment the line below for p2 when you implement equals
+//            // FixedSizeFIFOWorkList<E> other = (FixedSizeFIFOWorkList<E>) obj;
+//
+//            // Your code goes here
+//
+//            throw new NotYetImplementedException();
+//        }
+        if (obj == null || this != obj) {
             return false;
-        } else {
-            // Uncomment the line below for p2 when you implement equals
-            // FixedSizeFIFOWorkList<E> other = (FixedSizeFIFOWorkList<E>) obj;
-
-            // Your code goes here
-
-            throw new NotYetImplementedException();
+        } else if (obj instanceof CircularArrayFIFOQueue == false) {
+            return false;
         }
+
+        CircularArrayFIFOQueue<E> otherCopy = (CircularArrayFIFOQueue<E>) obj;
+        // FixedSizeFIFOWorkList<E> currentClone = this;
+
+        if (this.size() != otherCopy.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < otherCopy.size(); i++) {
+            if (otherCopy.next() != this.array[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
